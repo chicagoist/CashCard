29024597dd2821db303f017a5a12a0166bfdf032
chicagoist <le.legioner@gmail.com>
2024-08-01 13:55:15 +0200

    Spring Security

--- src/main/java/example/cashcard/CashCardController.java
+++ src/main/java/example/cashcard/CashCardController.java
@@ -11,6 +11,7 @@ import org.springframework.web.util.UriComponentsBuilder;
 import java.net.URI;
 import java.util.List;
 import java.util.Optional;
+import java.security.Principal;
 
 @RestController
 @RequestMapping("/cashcards")
@@ -22,29 +23,20 @@ class CashCardController {
     }
 
     @GetMapping("/{requestedId}")
-    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId) {
-
-        Optional<CashCard> cashCardOptional =
-                cashCardRepository.findById(requestedId);
-
-        if(cashCardOptional.isPresent()) {
+    private ResponseEntity<CashCard> findById(@PathVariable Long requestedId, Principal principal) {
+        Optional<CashCard> cashCardOptional = Optional
+                .ofNullable(cashCardRepository.findByIdAndOwner(requestedId, principal.getName()));
+        if (cashCardOptional.isPresent()) {
             return ResponseEntity.ok(cashCardOptional.get());
         } else {
             return ResponseEntity.notFound().build();
         }
-
-
-/*        if(requestedId.equals(99L)) {
-            CashCard cashCard = new CashCard(99L, 123.45);
-            return ResponseEntity.ok(cashCard);
-        } else {
-            return ResponseEntity.notFound().build();
-        }*/
     }
 
     @PostMapping
-    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb) {
-        CashCard savedCashCard = cashCardRepository.save(newCashCardRequest);
+    private ResponseEntity<Void> createCashCard(@RequestBody CashCard newCashCardRequest, UriComponentsBuilder ucb, Principal principal) {
+        CashCard cashCardWithOwner = new CashCard(null, newCashCardRequest.amount(), principal.getName());
+        CashCard savedCashCard = cashCardRepository.save(cashCardWithOwner);
         URI locationOfNewCashCard = ucb
                 .path("cashcards/{id}")
                 .buildAndExpand(savedCashCard.id())
@@ -52,79 +44,13 @@ class CashCardController {
         return ResponseEntity.created(locationOfNewCashCard).build();
     }
 
-//    @GetMapping()
-//    private ResponseEntity<Iterable<CashCard>> findAll() {
-//        return ResponseEntity.ok(cashCardRepository.findAll());
-//    }
-
     @GetMapping
-    private ResponseEntity<List<CashCard>> findAll(Pageable pageable) {
-        Page<CashCard> page = cashCardRepository.findAll(
+    private ResponseEntity<List<CashCard>> findAll(Pageable pageable, Principal principal) {
+        Page<CashCard> page = cashCardRepository.findByOwner(principal.getName(),
                 PageRequest.of(
                         pageable.getPageNumber(),
                         pageable.getPageSize(),
-                        pageable.getSortOr(Sort.by(Sort.Direction.ASC,
-                                "amount"))
-                ));
+                        pageable.getSortOr(Sort.by(Sort.Direction.ASC, "amount"))));
         return ResponseEntity.ok(page.getContent());
     }
 }
-
-/*
-6: Summary
-Congratulations! In this lesson you learned how to use test driven development
-to create your first Family Cash Card REST endpoint: a GET that returns a
-CashCard of a certain ID.
-* */
-
-/*
-8: Summary
-You've now successfully refactored the way the Family Cash Card API manages
-its data. Spring Data is now creating an in-memory H2 database and loading it
- with test data, which our tests utilize to exercise our API.
-
-Furthermore, we didn't change any of our tests! They actually guided us to a
-correct implementation. How awesome is that?!
-
-
-Learning Moment: main vs test resources
-Have you noticed that src/main/resources/schema.sql and
-src/test/resources/data.sql are in different resources locations?
-
-Can you guess why this is?
-
-Remember that our Cash Card with ID 99 and Amount 123.45 is a fake, made-up
-Cash Card that we only want to use in our tests. We don't want our "real" or
-production system to load Cash Card 99 into the system... what would happen
-to the real Cash Card 99?
-
-Spring has provided a powerful feature for us: it allows us to separate our
-test-only resources from our main resources when needed.
-
-Our scenario here is a common example of this: our database schema is always
-the same, but our data is not!
-
-Thanks again, Spring!
-* */
-
-/*
-8: Summary
-In this lab you learned how simple it is to add another endpoint to our API
--- the POST endpoint. You also learned how to use that endpoint to create and
- save a new CashCard to our database using Spring Data. Not only that, but
- the endpoint accurately implements the HTTP POST specification, which we
- verified using test driven development. The API is starting to be useful!
-
- */
-
-/*
-9: Summary
-In this lab, we implemented a "GET many" endpoint and added sorting and
-pagination. These accomplished two things:
-
-Ensured that the data received from the server is in a predictable and
-understandable order.
-Protected the client and server from being overwhelmed by a large amount of
-data (the page size puts a cap on the amount of data that can be returned in
-a single response).
-*/
--- src/test/java/example/cashcard/CashCardJsonTest.java
+++ src/test/java/example/cashcard/CashCardJsonTest.java
@@ -1,5 +1,6 @@
 package example.cashcard;
 
+
 import org.assertj.core.util.Arrays;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -25,9 +26,9 @@ class CashCardJsonTest {
     @BeforeEach
     void setUp() {
         cashCards = Arrays.array(
-                new CashCard(99L, 123.45),
-                new CashCard(100L, 1.00),
-                new CashCard(101L, 150.00));
+                new CashCard(99L, 123.45, "sarah1"),
+                new CashCard(100L, 1.00, "sarah1"),
+                new CashCard(101L, 150.00, "sarah1"));
     }
 
     @Test
@@ -38,8 +39,7 @@ class CashCardJsonTest {
         assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.id")
                 .isEqualTo(99);
         assertThat(json.write(cashCard)).hasJsonPathNumberValue("@.amount");
-        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@" +
-                        ".amount")
+        assertThat(json.write(cashCard)).extractingJsonPathNumberValue("@.amount")
                 .isEqualTo(123.45);
     }
 
@@ -48,28 +48,29 @@ class CashCardJsonTest {
         String expected = """
                 {
                     "id": 99,
-                    "amount": 123.45
+                    "amount": 123.45, 
+                    "owner": "sarah1"
                 }
                 """;
         assertThat(json.parse(expected))
-                .isEqualTo(new CashCard(99L, 123.45));
-        assertThat(json.parseObject(expected).id()).isEqualTo(99);
+                .isEqualTo(new CashCard(99L, 123.45, "sarah1"));
+        assertThat(json.parseObject(expected).id()).isEqualTo(99L);
         assertThat(json.parseObject(expected).amount()).isEqualTo(123.45);
     }
 
     @Test
     void cashCardListSerializationTest() throws IOException {
-        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list" +
-                ".json");
+        assertThat(jsonList.write(cashCards)).isStrictlyEqualToJson("list.json");
     }
 
     @Test
     void cashCardListDeserializationTest() throws IOException {
         String expected = """
                 [
-                   { "id": 99, "amount": 123.45 },
-                   { "id": 100, "amount": 1.00 },
-                   { "id": 101, "amount": 150.00 }
+                     {"id": 99, "amount": 123.45 , "owner": "sarah1"},
+                     {"id": 100, "amount": 1.00 , "owner": "sarah1"},
+                     {"id": 101, "amount": 150.00, "owner": "sarah1"}
+                                                  
                 ]
                 """;
         assertThat(jsonList.parse(expected)).isEqualTo(cashCards);
